9ae2ce08dba11858956040d232bd6486c36bf981
Aditi Surya <aditisurya1318@gmail.com>
2026-04-10 11:58:20 +0530

    Delete rag directory

--- rag/embeddings.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def create_embeddings(text_list):
-    embeddings = model.encode(text_list)
-    return embeddings
